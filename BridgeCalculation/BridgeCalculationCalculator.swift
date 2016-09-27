e38dfee298bb6ef5c7d59fb920dd76488bc1518a
Daniel <DerCSpringer@gmail.com>
2016-09-27 16:24:28 -0700

    Added penalties

---
+++ BridgeCalculation/BridgeCalculationCalculator.swift
@@ -0,0 +1,13 @@
+//
+//  BridgeCalculationCalculator.swift
+//  BridgeCalculation
+//
+//  Created by Daniel on 9/19/16.
+//  Copyright © 2016 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class BridgeCalculationCalculator: NSObject {
+
+}
